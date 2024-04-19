e4273e7c394e7f2f7529e71bc592ad8621398f58
pernilje <94057672+pernilje@users.noreply.github.com>
2024-04-19 23:20:06 +0200

    Den bedste kode nogensinde

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float speed = 5f;
+    Rigidbody _rb;
+    Vector2 _movement;  
+
+     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));   
+    }
+
+
+    private void FixedUpdate()
+    {
+        moveCharacter(_movement);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        _rb.AddForce(direction * speed);
     }
 }
