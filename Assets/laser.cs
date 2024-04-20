25de1c5974609d1b4f149037e1da15276a6ad6fa
Lowkee1g <59569937+Lowkee1g@users.noreply.github.com>
2024-04-20 12:12:38 +0200

    laser

--- Assets/laser.cs
+++ Assets/laser.cs
@@ -2,39 +2,21 @@ using UnityEngine;
 
 public class laser : MonoBehaviour
 {
-    public int maxReflectionCount = 5;
-    public float maxRayDistance = 100f;
-    public LayerMask layerMask; // Layer mask to selectively ignore colliders
+    // create a game object called bullet
+    public GameObject bullet;
 
-    private void Update()
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
     {
-        DrawReflectedRay(transform.position, transform.up, maxReflectionCount);
-    }
-
-    private void DrawReflectedRay(Vector2 position, Vector2 direction, int reflectionsRemaining)
-    {
-        if (reflectionsRemaining == 0)
-            return;
-
-        RaycastHit2D hit = Physics2D.Raycast(position, direction, maxRayDistance, layerMask);
-
-        // Draw the ray for debugging purposes
-        Debug.DrawRay(position, direction * maxRayDistance, Color.red);
-
-        if (hit.collider != null)
+        // throw the bullet in the direction of the laser using the bullet's rigidbody 2d   
+        if (Input.GetMouseButtonDown(0))
         {
-            // Draw the incoming ray
-            Debug.DrawLine(position, hit.point, Color.red);
-
-            // Reflect the ray off the surface
-            direction = Vector2.Reflect(direction, hit.normal);
-            position = hit.point;
-
-            // Draw the reflected ray from the hit point in the direction of reflection
-            Debug.DrawRay(position, direction * maxRayDistance, Color.green);
-
-            // Recursive call to draw the next ray
-            DrawReflectedRay(position, direction, reflectionsRemaining - 1);
+            GameObject bulletClone = Instantiate(bullet, transform.position, transform.rotation);
+            bulletClone.GetComponent<Rigidbody2D>().velocity = transform.up * 5;
         }
+        
     }
 }
